c4f369c238070508d76758770482fdfe0ed569ab
Wirak Lim <Wirak.Lim@jhuapl.edu>
2016-10-06 16:33:42 -0400

    Merge player and camera motion into one script.

--- Assets/Scripts/MainCameraControl.cs
+++ Assets/Scripts/MainCameraControl.cs
@@ -2,27 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//THIS SCRIPT NOT USED ANY LONGER
+
 public class MainCameraControl : MonoBehaviour {
 
 	public GameObject player;
 
 	private Vector3 offset;
-	private Vector3 lastLook;
 
 	void Start ()
 	{
 		offset = transform.position - player.transform.position;
-		lastLook = player.transform.forward;
 	}
 
 	void LateUpdate ()
 	{
-		transform.position = player.transform.position + offset;
+        float rotation = Input.GetAxis("Horizontal");
+
+        transform.position = player.transform.position + offset;
 		//transform.LookAt (player.transform.forward - Camera.main.transform.forward);
 		//Vector3 newLook = transform.TransformDirection (player.transform.forward);
 		Vector3 newLook = Vector3.Cross(player.transform.right, Vector3.up);
 		Debug.DrawRay (player.transform.position, newLook * 10, Color.green); 
-		transform.rotation = Quaternion.LookRotation (newLook);
-		Debug.DrawRay (player.transform.position, transform.forward * 10, Color.red); 
+		//transform.rotation = Quaternion.LookRotation (newLook);
+		Debug.DrawRay (player.transform.position, transform.forward * 10, Color.red);
+        transform.Rotate(0.0f, rotation, 0.0f);
 	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,21 +6,30 @@ public class PlayerController : MonoBehaviour {
 	public float speed;
 
 	private Rigidbody rb;
+    private Vector3 offset;
 
-	void Start ()
+    void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
-	}
+        offset = Camera.main.transform.position - transform.position;
+    }
 
-	void FixedUpdate ()
+    void FixedUpdate ()
 	{
 		float forwardMotion = Input.GetAxis ("Vertical");
 		float rotation = Input.GetAxis ("Horizontal");
 
 		Vector3 leftVector = Vector3.Cross (Camera.main.transform.right, Vector3.up);
-		Debug.DrawRay (transform.position, leftVector * 10, Color.blue);
+		//Debug.DrawRay (transform.position, leftVector * 10, Color.blue);
 		rb.AddTorque (Camera.main.transform.right * forwardMotion * speed);
-		//rb.AddForce (Camera.main.transform.forward * forwardMotion * speed);
-		transform.Rotate (transform.InverseTransformDirection(Vector3.up), rotation * speed);
+        //rb.AddForce (Camera.main.transform.forward * forwardMotion * speed);
+        transform.Rotate(0.0f, rotation*speed, 0.0f);
+        updateCamera(rotation);
 	}
+
+    void updateCamera(float rotation)
+    {
+        Camera.main.transform.position = transform.position + offset;
+        Camera.main.transform.Rotate(0.0f, rotation*speed, 0.0f);
+    }
 }
