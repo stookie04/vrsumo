4f295b83511db7f9fdcc5f15beaa57cc3b66729e
jinxzie <jinxzie@gmail.com>
2016-10-08 01:01:23 -0400

    added falling platform and player cage.

---
+++ Assets/Scripts/fall.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fall : MonoBehaviour {
+
+    float fallTime;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (tag == "Outer")
+            fallTime = 10.0f;
+        else if (tag == "Mid")
+            fallTime = 20.0f;
+
+	    if (Time.time > fallTime && transform.position.y > -25.0)
+        {
+            transform.Translate(Vector3.down * Time.deltaTime);
+        }
+
+        if (transform.position.y <= -25.0)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
