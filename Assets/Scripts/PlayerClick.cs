f3e8155c8a587d19448d28538c0d903f2ff3ba14
Lindsey DiAntonio <lindsey.diantonio@jhuapl.edu>
2016-11-17 16:28:39 -0500

    Lobby Scene

---
+++ Assets/Scripts/PlayerClick.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerClick : MonoBehaviour {
+	//public event Action OnClick;                                // Called when Fire1 is released and it's not a double click.
+	//public event Action OnDown;                                 // Called when Fire1 is pressed.
+	//public event Action OnUp;                                   // Called when Fire1 is released.
+	//public event Action OnDoubleClick;                          // Called when a double click is detected.
+	//public event Action OnCancel;                               // Called when Cancel is pressed.
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.anyKeyDown){
+			print (Input.inputString);
+		}
+	}
+}
