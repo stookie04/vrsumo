5841bcef7f7fabb4a3d169ecdacffbddf1dc810c
Wirak Lim <Wirak.Lim@jhuapl.edu>
2016-12-04 13:53:17 -0500

    Make ball follow head gaze.  Forward motion is automatic, and capped at speed (set in Player property).

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 
 public class PlayerController : NetworkBehaviour {
 
-	public float speed;
+	public float speed = 3.0f;
 
 	private Rigidbody rb;
 
@@ -20,38 +20,50 @@ public class PlayerController : NetworkBehaviour {
 
         head = (GvrHead)FindObjectOfType(typeof(GvrHead));
         head.transform.position = transform.position + offset;
-        head.transform.rotation = Quaternion.LookRotation(Vector3.zero - head.transform.position);
+        GvrViewer viewer = (GvrViewer)FindObjectOfType(typeof(GvrViewer));
+        viewer.Recenter();
+        //head.transform.rotation = Quaternion.LookRotation(Vector3.zero - head.transform.position);
         //Camera.main.transform.position = transform.position + offset;
         //Camera.main.transform.rotation = Quaternion.LookRotation(Vector3.zero - Camera.main.transform.position);
     }
 
-    void FixedUpdate ()
+    void Update ()
 	{
+
         if (!isLocalPlayer)
             return;
 
-        if (transform.position.y < 0.0f)
+        if (transform.position.y > 5.0f)
+        {
+            return;
+        }
+
+        if (transform.position.y < -0.0f)
         {
             rb.AddTorque(Vector3.zero);
             rb.isKinematic = true;
             return;
         }
 
-        float forwardMotion = Input.GetAxis ("Vertical");
+        //float forwardMotion = 10;// Input.GetAxis ("Vertical");
         float rotation = Input.GetAxis ("Horizontal");
 
-		//Vector3 leftVector = Vector3.Cross (Camera.main.transform.right, Vector3.up);
-		//Debug.DrawRay (transform.position, leftVector * 10, Color.blue);
-		rb.AddTorque (head.transform.right * forwardMotion * speed);
-        //rb.AddForce (Camera.main.transform.forward * forwardMotion * speed);
-        transform.Rotate(0.0f, rotation*speed, 0.0f);
+        //Vector3 leftVector = Vector3.Cross (Camera.main.transform.right, Vector3.up);
+        //Debug.DrawRay (transform.position, leftVector * 10, Color.blue);
+        //rb.AddTorque (head.Gaze.direction * 200f * Time.deltaTime);
+        if (rb.velocity.magnitude < speed)
+        {
+            rb.AddForce(head.Gaze.direction * 1000 * Time.deltaTime);
+        }
         updateCamera(rotation);
 	}
 
     void updateCamera(float rotation)
     {
         if (head)
+        {
             head.transform.position = transform.position + offset;
+        }
         //Camera.main.transform.position = transform.position + offset; 
         //Camera.main.transform.Rotate(0.0f, -rotation*speed, 0.0f);
     }
